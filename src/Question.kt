250a39b68f79839595ba6e756b4edc00e4c2decc
GlicH-ctrl <strenindenis@gmail.com>
2026-03-01 16:38:49 +0300

    end Question

--- src/Question.kt
+++ src/Question.kt
@@ -8,12 +8,57 @@ data class Question<T>(
     val difficulty: Difficulty
 )
 
-fun main() {
+object StudentProgress {
+    var total: Int = 0
+    var answered: Int = 0
+}
+
+class Quiz {
     val question1 = Question<String>("Речка спятила с ума – По домам пошла сама.", "водопровод", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("Земля круглая?", true, Difficulty.EASY)
     val question3 = Question<Int>("Сколько будет 2 + 2?", 4, Difficulty.HARD)
     
-    println(question1)
-    println(question2)
-    println(question3)
+    companion object StudentProgress {
+        var total: Int = 3
+        var answered: Int = 1
+    }
+    
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+    }
+}
+
+val Quiz.StudentProgress.progressText: String
+    get() = "${Quiz.answered} of ${Quiz.total} answered"
+
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("░") }
+    println()
+    println(Quiz.StudentProgress.progressText)
+}
+
+fun main() {
+    Quiz().apply {
+        printQuiz()
+    }
+    
+    println()
+    Quiz.StudentProgress.printProgressBar()
 }
